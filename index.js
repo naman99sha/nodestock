d94dd17456083683deccfa4dccbc6cbba2402d5e
Naman Sharma <naman99sha@gmail.com>
2022-02-17 11:47:27 +0530

    Added Post function and linked the form activity

--- index.js
+++ index.js
@@ -4,37 +4,37 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import request from 'request';
+import bodyParser from 'body-parser';
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 
-function call_api(finishedAPI){
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_3f83a57b74674152865e2f4a1534ab2a', {json: true}, (err, res, body) => {
+function call_api(tc, finishedAPI){
+    request('https://cloud.iexapis.com/stable/stock/'+tc+'/quote?token=pk_3f83a57b74674152865e2f4a1534ab2a', {json: true}, (err, res, body) => {
         if (err) {return console.log(err)}
         if (res.statusCode === 200) {finishedAPI(body);}  
     })
 }
-
-
-
-const PORT = process.env.PORT || 8080;
-const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    call_api(function(doneAPI) {
-        res.render('home', {
-            stock: doneAPI
+        res.render('home',{
+            home: true
         });
-    })
 });
 
 
 app.post('/', (req, res) => {
-    console.log(res)
-    call_api(function(doneAPI) {
+    var ticker = req.body.ticker;
+    call_api(ticker, function(doneAPI) {
         res.render('home', {
+            home: false,
             stock: doneAPI
         });
     })
